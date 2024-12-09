1ad9cb1adc635454263dc37fbbd74021ce638e11
Yann-afk <tzyyann07@gmail.com>
2024-12-09 14:52:38 +0700

    Complete assignment

--- test.py
+++ test.py
@@ -8,3 +8,13 @@ print (" may Allah bless us all")
 >>>>>>> 25a2126c9fbf40d03ffa95bac693ebeb1262cd69
 print("sadar lu botak")
 print("poke")
+# Input dari pengguna
+pinjaman_pokok = float(input("Masukkan jumlah pinjaman pokok: "))
+jangka_kredit = int(input("Masukkan jangka kredit dalam bulan: "))
+bagi_hasil_setahun = float(input("Masukkan bagi hasil per tahun (%): "))
+
+# Menghitung cicilan bulanan
+cicilan = (pinjaman_pokok, jangka_kredit, bagi_hasil_setahun)
+
+# Menampilkan hasil
+print(f"Cicilan per bulan yang harus dibayar Rp: {cicilan:, 2f}")
