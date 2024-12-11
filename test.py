19f7b19f85d01c44190c54d5bf3d66d9d724502e
Yann-afk <tzyyann07@gmail.com>
2024-12-11 22:51:26 +0700

    tugas uts

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#tugas uts Bagian 1 soal No 1
 import numpy as np
 
 # Matriks A dan B diberikan
@@ -27,3 +28,22 @@ print(B)
 
 print("\nHasil A x B:")
 print(C)
+
+#tugas uts Bagian Dua Soal No 1
+def irisan_himpunan(himpunan_a, himpunan_b):
+    # Konversi ke set untuk menghapus duplikasi
+    set_a = set(himpunan_a)
+    set_b = set(himpunan_b)
+    
+    # Cari irisan
+    irisan = set_a.intersection(set_b)
+    
+    return irisan
+
+# Data himpunan
+A = {3, 4, 5, 17, 2, 3, 5, 14, 3, 19}
+B = {4, 2, 5, 17, 2, 3, 6, 15, 6, 4, 20}
+
+# Panggil fungsi
+hasil_irisan = irisan_himpunan(A, B)
+print("Irisan dari A dan B adalah:", hasil_irisan)
