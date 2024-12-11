a4c324efb1ef11cbf907032c73f8344f268570bb
Yann-afk <tzyyann07@gmail.com>
2024-12-11 22:49:08 +0700

    tugas uts

--- test.py
+++ test.py
@@ -1,32 +1,29 @@
-print ("saya tampan")
-<<<<<<< HEAD
-print ("lukman")
-print ("sadar")
-=======
-print ("hello saya tampan im' lukman")
-print (" may Allah bless us all")
->>>>>>> 25a2126c9fbf40d03ffa95bac693ebeb1262cd69
-print("sadar lu botak")
-print("poke")
-# Input dari pengguna
-pinjaman_pokok = float(input("Masukkan jumlah pinjaman pokok: "))
-jangka_kredit = int(input("Masukkan jangka kredit dalam bulan: "))
-bagi_hasil_setahun = float(input("Masukkan bagi hasil per tahun (%): "))
+import numpy as np
 
-# Menghitung cicilan bulanan
-cicilan = (pinjaman_pokok, jangka_kredit, bagi_hasil_setahun)
+# Matriks A dan B diberikan
+A = np.array([
+    [10, 23, 21, 11],
+    [55, 5, 7, 88],
+    [34, 22, 21, 32],
+    [10, 14, 11, 15]
+])
+
+B = np.array([
+    [1, 0, 8, 1],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [1, 8, 0, 1]
+])
+
+# Perkalian matriks A x B
+C = np.dot(A, B)
 
 # Menampilkan hasil
-print(f"Cicilan per bulan yang harus dibayar Rp: {cicilan:, 2f}")
+print("Matrix A:")
+print(A)
 
-print("saya penyusup")
+print("\nMatrix B:")
+print(B)
 
-print('pengujian operasional')
-
-bil1 = input('masukan bilangan pertama:')
-bil2 = input('masukan bilangan kedua:')
-
-print (bil1,'>',  bil2,'?', bil1>bil2)
-print (bil1,'==', bil2,'?', bil1==bil2)
-print (bil1,'!=', bil2,'?', bil1!=bil2)
-print (bil1,'<=', bil2,'?', bil1<=bil2)
+print("\nHasil A x B:")
+print(C)
